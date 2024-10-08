b11ae1ad08da258cb672b8c96dac3d4073ec0b93
Dov Azogui <dov.azogui@gmail.com>
2024-10-08 15:40:22 -0400

    created the palindrom class

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+        
+        
+    def match(self, liste):
+        new_list = list()
+        
+        for words in liste:
+            if sorted(words) == sorted(self.word):
+                new_list.append(words)
+                
+        return new_list
+        
